ae15c32671d9cf1b1baa8f760de261a481f41569
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-08-30 22:13:15 -0300

    The ENDI.

--- Pokedex/ViewModels/DetailsVM.cs
+++ Pokedex/ViewModels/DetailsVM.cs
@@ -4,8 +4,7 @@ namespace Pokedex.ViewModels;
 
 public class DetailsVM
 {
-    public Pokemon Anterior { get; set; }        
-    public Pokemon Atual { get; set; }        
-    public Pokemon Proximo { get; set; }        
-
+    public Pokemon Anterior { get; set; }    
+    public Pokemon Atual { get; set; }     
+    public Pokemon Proximo { get; set; }
 }
